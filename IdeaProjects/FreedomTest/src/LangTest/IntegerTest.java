82f4617d39a833de7c3363bf7a0f19b67c02d854
Silverados <295843706@qq.com>
2018-04-10 14:41:06 +0800

    First commit

---
+++ IdeaProjects/FreedomTest/src/LangTest/IntegerTest.java
@@ -0,0 +1,14 @@
+package LangTest;
+
+public class IntegerTest {
+    public static void main(String[] args){
+        /**
+         *Integer(int value)构造一个新分配的 Integer对象，该对象表示指定的 int值。
+         *Integer(String s)构造一个新分配 Integer对象，表示 int由指示值 String参数。
+         */
+        Integer a = new Integer(1);
+        Integer b = new Integer("2");
+        Integer i = 9;
+
+    }
+}
--- readme.txt
+++ readme.txt
@@ -9,3 +9,5 @@ hello1
 =======
 hello1
 >>>>>>> fea
+
+git有个最佳实践，master是主分支，用来做正式发布版之后的保留历史，其他分支包括dev用来做正常开发，多个feature用来做某些特性功能，release用来做发布版历史，每次发布都是用release打包，hotfix用来做发布版之后的一些及时迭代修复bug的工作。
